03538cb6a9ab389abb37d6bc6d62b5c1a10d5c92
qwerty258 <zyf26256@gmail.com>
2018-02-21 21:53:47 +0800

    print out renaming tip

--- renamet.py
+++ renamet.py
@@ -7,6 +7,7 @@ import libtorrent
 def main():
     nametorrent=libtorrent.torrent_info(libtorrent.bdecode(open(sys.argv[1], 'rb').read())).name()+".torrent"
     tip="renaming {} to {}".format(sys.argv[1], nametorrent)
+    print(tip)
     os.rename(sys.argv[1], nametorrent)
 
 if __name__ == "__main__":
